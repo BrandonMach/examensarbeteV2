ede97a4e3ff1b3998d65fe32f17459a8805c1b46
BrandonMach <brandon.mach2003@gmail.com>
2024-02-08 21:14:26 +0100

    Commented Stopwatch code

--- Assets/Stopwatch shit/Stopwatch Scripts/StopwatchManager.cs
+++ Assets/Stopwatch shit/Stopwatch Scripts/StopwatchManager.cs
@@ -48,7 +48,9 @@ public class StopwatchManager : MonoBehaviour
         _timerHUDElement.text = _stopwatch.ToString("0.0");
 
 
-        if(!TimeRanOut){
+        // As long as Time hasn't run out reduce the stopwatch
+        if(!TimeRanOut)
+        {
             _stopwatch -= Time.deltaTime;
         }
 
@@ -56,6 +58,7 @@ public class StopwatchManager : MonoBehaviour
         AddMoreTime();
 
 
+        //Check if stopwatch time is done 
         if (_stopwatch <= 0 && !TimeRanOut)
         {
             TimeRanOut = true;
@@ -68,7 +71,8 @@ public class StopwatchManager : MonoBehaviour
 
 
 
-
+        //Will be chnaged later for multiplayer
+        //If time hasnät run out yet and player hasn't stopped time. Space to stop the time.
         if (!TimeRanOut && !player1Stopped && Input.GetKeyDown(KeyCode.Space) )
         {
 
@@ -79,7 +83,8 @@ public class StopwatchManager : MonoBehaviour
 
             _player1TimerText.text = playerStopTime.ToString("0.00");
             
-            if (playerStopTime < 2 && playerStopTime > 0)
+            //For testing, in real game its the player that is the closest to 0 that wins
+            if (playerStopTime < 2 && playerStopTime > 0) 
             {
                 Debug.LogError("You Won");
             }
@@ -94,33 +99,36 @@ public class StopwatchManager : MonoBehaviour
 
     void AddMoreTime()
     {
+        // Multiply so that loudness easier to work with
         loudness = _audioDetector.GetLoudnessFromMicrophone() * loudnessSensibility;
 
-        
+        //if loudness is negative set to 0 as mininum and prevent more time to be added until it has atleast reseted
         if(loudness < threshold)
         {
             loudness = 0;
             _addMoreTime = false;
         }
 
+        //if loudness is more than threashold and hasn't added time => add extra time and spawn HUD element that shows how much time got added
         if (loudness > threshold && !_addMoreTime)
         {
             _addMoreTime = true;
             _stopwatch += _timeToAdd;
 
 
-            
+            #region Add Time HUD visual
 
             Debug.LogError("Add " + _timeToAdd + " seconds");
             GameObject tempAddTimePrefab = Instantiate(_timeToAddPrefab);
             tempAddTimePrefab.transform.parent = _canvas.transform;
             tempAddTimePrefab.transform.localScale = new Vector3(1, 1, 1);
             tempAddTimePrefab.transform.localPosition = new Vector3(80, 140, 0);
-            //Destroy(tempAddTimePrefab, 1f);
+
+            #endregion
         }
-        
-        
-        
+
+
+
     }
 
     
