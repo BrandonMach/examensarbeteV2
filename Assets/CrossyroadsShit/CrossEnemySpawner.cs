a0a8bb495318b15d1895f683071274df94b91a26
AJervill <axeljervill@gmail.com>
2024-04-07 15:23:36 +0200

    continuation of crossyroads, character model interaction, and started the joycon implementation

--- Assets/CrossyroadsShit/CrossEnemySpawner.cs
+++ Assets/CrossyroadsShit/CrossEnemySpawner.cs
@@ -6,23 +6,41 @@ public class CrossEnemySpawner : MonoBehaviour
 {
     public List<GameObject> Enemies = new List<GameObject>();
     private float timer;
+    [SerializeField] bool interaction;
     public Vector3 rot, pos;
-    public int rotationDegree;
+    [SerializeField] int rotation;
+    [HideInInspector] public int rotationDegree;
     GameObject obstacles;
     public GameObject center;
 
     private void Start()
     {
         Spawn();
-        rotationDegree = 5;
+        rotationDegree = rotation;
         rot = new Vector3(0,1,0);
         pos = obstacles.GetComponent<Renderer>().bounds.center;
+        interaction = false;
         
     }
 
     private void FixedUpdate()
     {
         //obstacles.transform.RotateAround(position, rotation, rotationDegree * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.R) && !interaction)
+        {
+            interaction = true; 
+        }
+        if (interaction)
+        {
+            rotationDegree = rotation * -1;
+            timer += Time.deltaTime;
+            if (timer >= 8)
+            {
+                timer = 0;
+                interaction = false;
+                rotationDegree = rotation;
+            }
+        }
     }
     private void Spawn()
     {
